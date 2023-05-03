37acc66cacbe56a4f917fcdc5c0ce76ffa56cf64
Norbert Sosnowski <norbert.sosnowski.front@gmail.com>
2023-05-03 15:28:03 +0200

    Little refactor of notes store
    Removing unnecessary code.

--- src/components/stores/notes.ts
+++ src/components/stores/notes.ts
@@ -3,7 +3,7 @@ import { computed, ref } from "vue";
 import { add48Hours } from "./tools/add48Hours";
 import { checkForExpiredNotes } from "./tools/checkForExpiredNotes";
 
-type filterType = "all" | "active" | "toExpired";
+type filterType = "all" | "active" | "toDelete";
 
 export interface notes {
   text: string;
@@ -16,39 +16,44 @@ const piniaStoreName = "notes";
 
 export const useNotesStore = defineStore(piniaStoreName, () => {
   // states
+  const filter = ref<filterType>("all");
   const notesOBJ = ref([] as NotesArray);
   const notesJSON = localStorage.getItem(piniaStoreName);
   if (notesJSON != null) {
     notesOBJ.value = JSON.parse(notesJSON);
 
+    // test note
     notesOBJ.value.push([{ text: "Test1", creationDate: new Date() }]);
 
     notesOBJ.value = checkForExpiredNotes(notesOBJ.value);
   }
-  const filter = ref<filterType>("all");
 
   // getters
   const filteredNotes = computed(() => {
     switch (filter.value) {
       case "all":
-        return notesOBJ.value;
+        return notesOBJ;
       case "active":
-        return notesOBJ.value.forEach((notesGroup) => {
-          notesGroup.filter((note) => note.dateToDelete === undefined);
+        notesOBJ.value.forEach((notesGroup) => {
+          notesGroup = notesGroup.filter(
+            (note) => note.dateToDelete === undefined
+          );
         });
-      case "toExpired":
-        return notesOBJ.value.forEach((notesGroup) => {
-          notesGroup.filter((note) => note.dateToDelete !== undefined);
+        return notesOBJ;
+      case "toDelete":
+        notesOBJ.value.forEach((notesGroup) => {
+          notesGroup = notesGroup.filter(
+            (note) => note.dateToDelete !== undefined
+          );
         });
-      default:
-        throw new Error("Unknown value of filter.value");
+        return notesOBJ;
     }
   });
 
   // actions
   function createNewNote(
     { text, creationDate: dataCreation }: notes,
-    level: unknown[]
+    level: notes[]
   ) {
     /*
     level argument is here as level access, when code calls it, it pushes the change directly into the level it wants
@@ -67,24 +72,13 @@ export const useNotesStore = defineStore(piniaStoreName, () => {
     level.push({ text, creationDate: dataCreation });
   }
 
-  function markNoteToDelete(note: notes, array: unknown[]) {
-    if (array.indexOf(note) === -1) {
-      // didn't find the note
-      for (const lowerArrayLevel of array) {
-        markNoteToDelete(note, lowerArrayLevel as unknown[]);
-      }
-    } else if (!("length" in array)) {
-      // break the call loop if the array points on note object
-      return;
-    } else {
-      // found the note
-      const index = array.indexOf(note);
-      if (index === -1)
-        throw new Error(`Cannot find the note of text ${note.text}`);
-      const expireDate = add48Hours(new Date());
+  function markNoteToDelete(note: notes, array: notes[]) {
+    const index = array.indexOf(note);
+    if (index === -1)
+      throw new Error(`Cannot find the note of text ${note.text}`);
+    const expireDate = add48Hours(new Date());
 
-      (array[index] as notes).dateToDelete = expireDate;
-    }
+    array[index].dateToDelete = expireDate;
   }
 
   function forceNotesDeletion() {
